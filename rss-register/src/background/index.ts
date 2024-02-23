5760210d1da4bb33eaaa3f4b6378b0a3da7ee126
kazuma takiuchi <basstrombone413@gmail.com>
2024-02-23 23:29:22 +0900

    環境変数設定修正

--- rss-register/src/background/index.ts
+++ rss-register/src/background/index.ts
@@ -1,15 +1,13 @@
 import Parser from "rss-parser";
 import { initializeApp } from "firebase/app";
-import "dotenv/config";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 {
@@ -29,6 +27,9 @@ const firebaseConfig = {
 
       // Initialize Firebase
       const app = initializeApp(firebaseConfig);
+      console.log(app);
+      console.log(import.meta.env);
+      console.log(firebaseConfig);
 
       //   feed.items.forEach((item) => {
       //     console.log(item.title + ":" + item.link);
