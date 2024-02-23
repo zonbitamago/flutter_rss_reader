8995ca17146ebfc7ee121adfda25b95694c03ec0
kazuma takiuchi <basstrombone413@gmail.com>
2024-02-23 23:03:09 +0900

    firebase設定追加

--- rss-register/src/background/index.ts
+++ rss-register/src/background/index.ts
@@ -1,4 +1,16 @@
 import Parser from "rss-parser";
+import { initializeApp } from "firebase/app";
+import "dotenv/config";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
 
 {
   chrome.action.onClicked.addListener(async (tab) => {
@@ -14,6 +26,10 @@ import Parser from "rss-parser";
       const parser = new Parser();
       const feed = await parser.parseURL(url);
       console.log(feed.title);
+
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+
       //   feed.items.forEach((item) => {
       //     console.log(item.title + ":" + item.link);
       //   });
