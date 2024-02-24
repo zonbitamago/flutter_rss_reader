170c90e20d1be29f303dd869d990f1068c7bd95c
kazuma takiuchi <basstrombone413@gmail.com>
2024-02-24 09:34:34 +0900

    firebaseconfig追加

--- rss_reader/lib/main.dart
+++ rss_reader/lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:rss_dart/dart_rss.dart';
 import 'package:rss_dart/domain/rss1_feed.dart';
 
-void main() {
+void main() async {
+  final configurations = Configurations();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: configurations.apiKey,
+          appId: configurations.appId,
+          messagingSenderId: configurations.messagingSenderId,
+          projectId: configurations.projectId));
+
   runApp(const MyApp());
 }
 
